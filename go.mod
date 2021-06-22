e5797942813c2c2696e857d4f048c852a560cbc9
xyming108 <1931559710@qq.com>
2021-06-22 15:20:44 +0800

    update

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.16
 
 require (
 	github.com/garyburd/redigo v1.6.2
-	github.com/go-redis/redis v6.15.9+incompatible // indirect
+	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/julienschmidt/httprouter v1.3.0
+	github.com/onsi/ginkgo v1.16.4 // indirect
+	github.com/onsi/gomega v1.13.0 // indirect
 )
--- test.go
+++ test.go
@@ -1,13 +1,29 @@
+//brief_intro/echo.go
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+var logger = log.New(os.Stdout, "", 0)
+
+func echo(wr http.ResponseWriter, r *http.Request) {
+	timeStart := time.Now()
+	_, err := wr.Write([]byte("hello"))
+	timeEnd := time.Since(timeStart)
+	logger.Println(timeEnd)
+	if err != nil {
+		return
+	}
+}
 
 func main() {
-	var a = new(int)
-	*a = 100
-	fmt.Println(*a)
-
-	var b = make(map[string]int, 10)
-	b["沙河娜扎"] = 100
-	fmt.Println(b)
+	http.HandleFunc("/", echo)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
