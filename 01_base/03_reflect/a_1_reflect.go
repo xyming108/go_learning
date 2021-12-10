52d86251aa0defd7b05eda3ab0ea28fa39334c72
xyming108 <1931559710@qq.com>
2021-12-10 14:46:56 +0800

    1

--- 01_base/03_reflect/a_1_reflect.go
+++ 01_base/03_reflect/a_1_reflect.go
@@ -17,7 +17,7 @@ func reflectType(x interface{}) {
 	//1、方式一：通过类型断言
 	//2、方式二：借助反射
 	obj := reflect.TypeOf(x)
-	fmt.Println(obj, obj.Name(), obj.Kind())
+	fmt.Println(obj, obj.Name(), obj.Kind(), obj.Size(), obj.String())
 	fmt.Printf("%T\n", obj)
 }
 
@@ -28,7 +28,6 @@ func reflectValue(x interface{}) {
 	//如何得到一个传入时候类型的变量
 	switch k {
 	case reflect.Float32:
-		//把反射取到的值转换成一个int32类型的变量
 		ret := float32(v.Float())
 		fmt.Printf("%v %T\n", ret, ret)
 	case reflect.Int32:
@@ -43,17 +42,18 @@ func reflectValue1(x interface{}) {
 	k := v.Elem().Kind() //拿到值对应的类型种类
 	switch k {
 	case reflect.Float32:
-		//把反射取到的值转换成一个int32类型的变量
 		v.Elem().SetFloat(3.14)
 	case reflect.Int32:
-		v.Elem().SetInt(100)
+		v.Elem().SetInt(1000)
 	}
 }
 
 type Cat struct {
+	name string
 }
 
 type Dog struct {
+	age int
 }
 
 func main() {
@@ -82,9 +82,12 @@ func main() {
 	reflectValue(h)
 	fmt.Println()
 
+	fmt.Println("----------")
 	var i int32 = 10
 	reflectValue1(&i)
-	fmt.Println(i)
+	fmt.Printf("%v %T\n", i, i)
+	fmt.Println("-----------")
+
 	//IsNil用于判断指针是否为空，IsValid用于判断返回值是否有效
 	var j *int
 	fmt.Println(reflect.ValueOf(j).IsNil())
--- test.go
+++ test.go
@@ -1,10 +1,30 @@
 package main
 
 import (
-	"context"
+	"fmt"
+	"reflect"
 )
 
-func main() {
-	context.WithCancel(context.Background())
+func Add(a, b int) int {
+	return a + b
+}
 
+func main() {
+	v := reflect.ValueOf(Add)
+	if v.Kind() != reflect.Func {
+		return
+	}
+	t := v.Type()
+	argv := make([]reflect.Value, t.NumIn())
+	for i := range argv {
+		if t.In(i).Kind() != reflect.Int {
+			return
+		}
+		argv[i] = reflect.ValueOf(i)
+	}
+	result := v.Call(argv)
+	if len(result) != 1 || result[0].Kind() != reflect.Int {
+		return
+	}
+	fmt.Println(result[0].Int()) // #=> 1
 }
