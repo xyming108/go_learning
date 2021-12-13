53d84db994e49f4793a97823dd723d0e19db9c60
xyming108 <1931559710@qq.com>
2021-12-13 09:09:07 +0800

    b

--- 01_base/13_go_json/1_encode.go
+++ 01_base/13_go_json/1_encode.go
@@ -1,1 +1,80 @@
-package _3_go_json
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+func ExampleMarshal() {
+	type ColorGroup struct {
+		ID     int
+		Name   string
+		Colors []string
+	}
+	group := ColorGroup{
+		ID:     1,
+		Name:   "Reds",
+		Colors: []string{"Crimson", "Red", "Ruby", "Maroon"},
+	}
+	b, err := json.Marshal(group)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	os.Stdout.Write(b)
+	fmt.Println()
+	fmt.Println(string(b))
+	// Output:
+	// {"ID":1,"Name":"Reds","Colors":["Crimson","Red","Ruby","Maroon"]}
+}
+
+func ExampleMarshalIndent() {
+	/*data := map[string]int{
+		"a": 1,
+		"b": 2,
+	}*/
+
+	type ColorGroup struct {
+		ID     int
+		Name   string
+		Colors []string
+	}
+	group := ColorGroup{
+		ID:     1,
+		Name:   "Reds",
+		Colors: []string{"Crimson", "Red", "Ruby", "Maroon"},
+	}
+
+	//b, err := json.MarshalIndent(data, "<prefix>", "<indent>")
+	b, err := json.MarshalIndent(group, "", "\t")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(b))
+	// Output:
+	// {
+	// <prefix><indent>"a": 1,
+	// <prefix><indent>"b": 2
+	// <prefix>}
+
+	/*
+		{
+		        "ID": 1,
+		        "Name": "Reds",
+		        "Colors": [
+		                "Crimson",
+		                "Red",
+		                "Ruby",
+		                "Maroon"
+		        ]
+		}
+	*/
+}
+
+func main() {
+	ExampleMarshal()
+	fmt.Printf("\n\n")
+	ExampleMarshalIndent()
+}
--- 01_base/13_go_json/2_decode.go
+++ 01_base/13_go_json/2_decode.go
@@ -1,1 +1,61 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"strings"
+)
+
+func ExampleUnmarshal() {
+	var jsonBlob = []byte(`[
+	{"Name": "Platypus", "Order": "Monotremata"},
+	{"Name": "Quoll",    "Order": "Dasyuromorphia"}]`)
+	type Animal struct {
+		Name  string
+		Order string
+	}
+	var animals []Animal
+	err := json.Unmarshal(jsonBlob, &animals)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	fmt.Printf("%+v", animals)
+	// Output:
+	// [{Name:Platypus Order:Monotremata} {Name:Quoll Order:Dasyuromorphia}]
+}
+
+func ExampleDecoder() {
+	const jsonStream = `
+	{"Name": "Ed", "Text": "Knock knock."}
+	{"Name": "Sam", "Text": "Who's there?"}
+	{"Name": "Ed", "Text": "Go fmt."}
+	{"Name": "Sam", "Text": "Go fmt who?"}
+	{"Name": "Ed", "Text": "Go fmt yourself!"}`
+	type Message struct {
+		Name, Text string
+	}
+	dec := json.NewDecoder(strings.NewReader(jsonStream))
+	for {
+		var m Message
+		if err := dec.Decode(&m); err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s: %s\n", m.Name, m.Text)
+	}
+	// Output:
+	// Ed: Knock knock.
+	// Sam: Who's there?
+	// Ed: Go fmt.
+	// Sam: Go fmt who?
+	// Ed: Go fmt yourself!
+}
+
+func main() {
+	ExampleUnmarshal()
+	fmt.Printf("\n\n")
+	ExampleDecoder()
+}
