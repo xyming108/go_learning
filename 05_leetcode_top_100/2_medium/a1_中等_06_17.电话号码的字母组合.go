cb30e599050bbb798e3be4beeaa22e10572eda2e
xyming108 <1931559710@qq.com>
2022-04-08 09:26:17 +0800

    1

---
+++ 05_leetcode_top_100/2_medium/a1_中等_06_17.电话号码的字母组合.go
@@ -0,0 +1,65 @@
+package main
+
+import "fmt"
+
+/*
+给定一个仅包含数字2-9的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+
+示例 1：
+输入：digits = "23"
+输出：["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+示例 2：
+输入：digits = ""
+输出：[]
+
+示例 3：
+输入：digits = "2"
+输出：["a","b","c"]
+*/
+
+/*
+回溯法
+时间复杂度：O(3^m × 4^n)
+空间复杂度：O(m+n)
+*/
+var phoneMap map[string]string = map[string]string{
+	"2": "abc",
+	"3": "def",
+	"4": "ghi",
+	"5": "jkl",
+	"6": "mno",
+	"7": "pqrs",
+	"8": "tuv",
+	"9": "wxyz",
+}
+
+var combinations []string
+
+func letterCombinations(digits string) []string {
+	if len(digits) == 0 {
+		return []string{}
+	}
+	combinations = []string{}
+	backtrack(digits, 0, "")
+	return combinations
+}
+
+func backtrack(digits string, index int, combination string) {
+	if index == len(digits) {
+		combinations = append(combinations, combination)
+	} else {
+		digit := string(digits[index])
+		letters := phoneMap[digit]
+		lettersCount := len(letters)
+		for i := 0; i < lettersCount; i++ {
+			backtrack(digits, index+1, combination+string(letters[i]))
+		}
+	}
+}
+
+func main() {
+	digits := "234"
+	fmt.Println(letterCombinations(digits))
+}
