567b649f85d030ee10df2c54cfe9252b4824a998
xyming108 <1931559710@qq.com>
2022-04-09 22:47:34 +0800

    1

--- 05_leetcode_top_100/2_medium/a1_中等_08_22.括号生成.go
+++ 05_leetcode_top_100/2_medium/a1_中等_08_22.括号生成.go
@@ -1,7 +1,9 @@
 package main
 
+import "fmt"
+
 /*
-数字 n代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
 
 示例 1：
 输入：n = 3
@@ -12,10 +14,28 @@ package main
 输出：["()"]
 */
 
+/*
+回溯法
+*/
 func generateParenthesis(n int) []string {
-
+	var result []string
+	var backtrace func(left, right int, str string)
+	backtrace = func(leftRemain, rightRemain int, str string) {
+		if len(str) == 2*n { //递归出口
+			result = append(result, str)
+			return
+		}
+		if leftRemain > 0 { //剪枝条件1
+			backtrace(leftRemain-1, rightRemain, str+"(")
+		}
+		if leftRemain < rightRemain { //剪枝条件2
+			backtrace(leftRemain, rightRemain-1, str+")")
+		}
+	}
+	backtrace(n, n, "")
+	return result
 }
 
 func main() {
-
+	fmt.Println(generateParenthesis(3))
 }
